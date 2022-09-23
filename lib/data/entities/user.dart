eb8f5da89a6753b67af2fdd52222d5556506776c
Ignacio López Otero <ignacio.lopezotero@rootstrap.com>
2022-09-23 11:49:28 -0300

    approach

--- lib/data/entities/user.dart
+++ lib/data/entities/user.dart
@@ -14,4 +14,26 @@ class User {
     required this.gender,
     this.avatar,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': this.id,
+      'email': this.email,
+      'fullName': this.fullName,
+      'username': this.username,
+      'gender': this.gender,
+      'avatar': this.avatar,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id'] as int,
+      email: map['email'] as String,
+      fullName: map['fullName'] as String,
+      username: map['username'] as String,
+      gender: map['gender'] as String,
+      avatar: map['avatar'] as String,
+    );
+  }
 }
--- lib/data/services/user_service.dart
+++ lib/data/services/user_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_target/data/entities/user.dart';
 import 'package:flutter_target/data/utils/net_constants.dart';
 import 'package:flutter_target/domain/repositories/user_repository.dart';
 
@@ -13,9 +14,31 @@ class UserService implements UserRepository {
 
   @override
   void signUp(String username, String email, String gender, String password,
-      String confirmPassword) {
+      String confirmPassword) async {
     try {
-      //TODO
+      final dio = getDio();
+
+      final Map<String, dynamic> request = {
+        "user": {
+          "username": username,
+          "email": email,
+          "password": password,
+          "confirm_password": confirmPassword,
+          "gender": gender,
+        }
+      };
+
+      final user = User(
+        id: 1,
+        email: email,
+        fullName: password,
+        username: username,
+        gender: gender,
+      );
+
+      final response = await dio.post(NetConstants.signUp, data: request);
+
+      print(response);
     } catch (e) {
       //TODO
     }
